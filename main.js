2a25efc7cc9695ba5c8d868a97a2ec0d004f88ec
chanpi23 <chandrikad34@gmail.com>
2022-06-27 22:16:41 -0400

    jquery finished

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $("#title").autocomplete({
+    source: async function (request, response) {
+      let data = await fetch(
+        `http://localhost:8000/search?query=${request.term}`
+      )
+        .then((results = results.json()))
+        .then((results) =>
+          results.map((results) => {
+            return {
+              label: results.title,
+              value: results.title,
+              id: results._id,
+            };
+          })
+
+        )
+        response(data)
+    },
+    minLength:2,
+    select: function(event,ui) {
+        console.log(ui.item.id)
+        fetch(`http://localhost:3000/get/${ui.item.id}`)
+        .then(result => result.json())
+        .then(results => {
+            $('#cast').empty()
+            result.cast.forEach (cast =>{
+                $(cast).append(`<li>${cast}</li>`)
+            })
+            $('img').attr('src', results.poster)
+        })
+    }
+  });
+});
